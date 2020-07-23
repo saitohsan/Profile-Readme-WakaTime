3cfd232a99db8198f330a7812a7ad80f0ccb5f88
Avinal <avinal.xlvii@gmail.com>
2020-07-23 11:49:37 +0530

    initial comit

---
+++ main.py
@@ -0,0 +1,30 @@
+'''
+WakaTime progress visualizer
+Original File Location https://github.com/athul/waka-readme
+'''
+
+import re
+import os
+import sys
+import base64
+import datetime
+import requests
+from github import Github, GithubException
+
+user = 'avinal'
+waka_key = os.getenv('INPUT_WAKATIME_API_KEY')
+ghtoken = os.getenv('INPUT_GH_TOKEN')
+show_title = 'true'
+
+if __name__ == '__main__':
+    g = Github(ghtoken)
+    try:
+        repo = g.get_repo(f"{user}/{user}")
+    except GithubException:
+        print("Authentication Error. Try saving a GitHub Token in your Repo Secrets or Use the GitHub Actions Token, which is automatically used by the action.")
+        sys.exit(1)
+    contents = repo.get_contents("image/some.txt",ref="test")
+    new_readme = open("new.txt","w")
+    new_readme.write("Hello cosmos")
+    repo.update_file(path=contents.path, message='Updated with Dev Metrics',
+                         content=new_readme, sha=contents.sha, branch='master')
