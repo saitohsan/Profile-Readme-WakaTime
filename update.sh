dedf39943f30605dc9d684f8f3009fbe6506aa75
Avinal <avinal.xlvii@gmail.com>
2020-07-24 11:32:06 +0530

    worked but not

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@ git config user.email "185067@nith.ac.in"
 git config user.name "avinal"
 python main.py
 cp new.txt image
-git add new.txt
+git add .
 git commit -m "Bot updated the file"
 git push origin master
