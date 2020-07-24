df57ca864601feb386615c11bcdd063ab0bd7e05
Avinal <avinal.xlvii@gmail.com>
2020-07-24 23:42:12 +0530

    variable renamed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
-pip3 install requirements.txt
-python3 main.py
-
 if [ -z "${INPUT_GITHUB_TOKEN}" ]; then
     echo "error: not found GITHUB_TOKEN"
     exit 1
 fi
 
 remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+echo "$remote_repo"
 git config http.sslVerify false
 git config user.email "actions@users.noreply.github.com"
 git config user.name "The Bot Airium"
+echo "Added name email"
 git remote add publisher "${remote_repo}"
+echo "Add publisher"
 git show-ref
 git branch --verbose
 echo "Git config added"
