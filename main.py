80b580bc69da33cf1e618c7c4f10bebc7c2be487
Avinal <avinal.xlvii@gmail.com>
2020-07-24 10:43:57 +0530

    new approach

--- main.py
+++ main.py
@@ -1,28 +1,4 @@
-'''
-WakaTime progress visualizer
-Original File Location https://github.com/athul/waka-readme
-'''
-
-import re
-import os
-import sys
-import base64
-import datetime
-import requests
-from github import Github, GithubException
-
-user = 'avinal'
-ghtoken = os.getenv('INPUT_GH_TOKEN')
-show_title = 'true'
-
 if __name__ == '__main__':
-    g = Github(ghtoken)
-    try:
-        repo = g.get_repo(f"avinal/README-Python-Experiment")
-    except GithubException:
-        print("Authentication Error. Try saving a GitHub Token in your Repo Secrets or Use the GitHub Actions Token, which is automatically used by the action.")
-        sys.exit(1)
-    contents = repo.get_contents("image/some.txt")
     new_readme = open("new.txt","w")
     new_readme.write("Hello cosmos with me ")
     new_readme.close()
--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
+pip install -r requirements.txt
+python main.py
 git clone https://github.com/avinal/README-Python-Experiment.git 
+cp new.txt README-Python-Experiments/image
 cd README-Python-Experiment
-cp new.txt image
 git add new.txt
 git commit -m "Updated file"
 git push origin master
