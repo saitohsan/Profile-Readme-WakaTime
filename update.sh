fef718f9e0603ad268612b1a364e87a2a34a26ca
Avinal <avinal.xlvii@gmail.com>
2020-07-24 10:54:16 +0530

    checking

--- update.sh
+++ update.sh
@@ -1,8 +1,4 @@
 #! /bin/bash
 python main.py
-git clone https://github.com/avinal/README-Python-Experiment.git 
-cp new.txt README-Python-Experiments/image
-cd README-Python-Experiment
-git add new.txt
-git commit -m "Updated file"
-git push origin master
+cat new.txt
+pwd
