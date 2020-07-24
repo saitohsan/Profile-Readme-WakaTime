f915d3fa2e65a079ad6ea1ffd81930edc2a0aca3
Avinal <avinal.xlvii@gmail.com>
2020-07-24 11:48:08 +0530

    worked last timet

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 git config user.email "185067@nith.ac.in"
 git config user.name "avinal"
 python main.py
-cp new.txt image
+cp some.txt image
 git add .
 git commit -m "Bot updated the file"
 git push origin master
