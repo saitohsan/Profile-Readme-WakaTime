1957d666f28accfc7c84cc1edfb1e09ed1dd6b3e
Avinal <avinal.xlvii@gmail.com>
2020-07-24 18:09:47 +0530

    mewww

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 git config user.email "185067@nith.ac.in"
 git config user.name "avinal"
+pip install -r requirements.txt
 python main.py
 cp stat.svg image
-git add stat.svg
+git add "stat.svg"
 git commit -m "I didn't update the file :alien:"
 git push origin master
