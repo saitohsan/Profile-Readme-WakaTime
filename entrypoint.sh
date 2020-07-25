17ceabdb74a593f6e17bc607953e8fd620a08dcc
Avinal <avinal.xlvii@gmail.com>
2020-07-25 13:00:30 +0530

    Its working now

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,36 +1,32 @@
 #! /bin/bash
-pwd
 if [ -z "${INPUT_GITHUB_TOKEN}" ]; then
     echo "error: not found GITHUB_TOKEN"
     exit 1
 fi
+
 clone_repo="https://github.com/${GITHUB_ACTOR}/${GITHUB_ACTOR}.git"
 git clone "${clone_repo}"
+echo "Repository Cloned"
 
 if [[ ! -f "stat.svg" ]]; then
-    echo "File Lost"
+    echo "error: file lost! existing"
     exit 1
 fi
 
 if [[ ! -d "${GITHUB_ACTOR}/images" ]]; then
     mkdir "${GITHUB_ACTOR}/images"
-    echo "Images Folder created"
+    echo "images folder created"
 fi
 
 cp stat.svg "${GITHUB_ACTOR}/images"
-echo "Copied New Image"
+echo "copied new image"
 
 cd "${GITHUB_ACTOR}"
-ls images/
 
 remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-git config user.email "actions@users.noreply.github.com"
-git config user.name "github-actions[bot]"
-echo "Added name email"
-
-git show-ref
-git branch --verbose
-echo "Git config added"
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config user.name "GitHub Actions"
+echo "git credential added" 
 
 git remote add publisher "${remote_repo}"
 git remote -v
@@ -38,7 +34,7 @@ git remote -v
 git checkout master
 
 git add .
-git commit -m "Automated Update :alien:"
+git commit -m "Automated Coding Activity Update :alien:"
 git pull
 git push publisher master
-echo "Git Push Successful"
+echo "image update successfull"
--- main.py
+++ main.py
@@ -7,24 +7,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import requests
 
-waka_key = os.getenv('INPUT_WAKATIME_API_KEY')
+waka_key = os.getenv("INPUT_WAKATIME_API_KEY")
 
 
 def this_week() -> str:
-    '''Returns a week streak'''
+    """Returns a week streak"""
     week_end = datetime.datetime.today() - datetime.timedelta(days=1)
     week_start = week_end - datetime.timedelta(days=7)
-    print("Week header created")
-    return f"Week: {week_start.strftime('%d %B, %Y')} - {week_end.strftime('%d %B, %Y')}"
+    print("week header created")
+    return (
+        f"Week: {week_start.strftime('%d %B, %Y')} - {week_end.strftime('%d %B, %Y')}"
+    )
 
 
 def make_graph(data: list):
-    '''Make progress graph from API graph'''
-    fig, ax = plt.subplots(figsize=(10,2))
-    with open('/colors.json') as json_file:
+    """Make progress graph from API graph"""
+    fig, ax = plt.subplots(figsize=(10, 2))
+    with open("/colors.json") as json_file:
         color_data = json.load(json_file)
     y_pos = np.arange(len(data[0]))
-    bars = ax.barh(y_pos,data[2])
+    bars = ax.barh(y_pos, data[2])
     ax.set_yticks(y_pos)
     ax.get_xaxis().set_ticks([])
     ax.set_yticklabels(data[0])
@@ -33,37 +35,47 @@ def make_graph(data: list):
     plt.box(False)
     for i, bar in enumerate(bars):
         if data[0][i] in color_data:
-            bar.set_color(color_data[data[0][i]]['color'])
-        x_value= bar.get_width()
-        y_values=bar.get_y()+bar.get_height()/2
-        plt.annotate(data[1][i], (x_value,y_values),xytext=(4,0),textcoords="offset points", va='center',ha='left')
-    plt.savefig('stat.svg', bbox_inches='tight')
-    print("New Image Generated")
+            bar.set_color(color_data[data[0][i]]["color"])
+        x_value = bar.get_width()
+        y_values = bar.get_y() + bar.get_height() / 2
+        plt.annotate(
+            data[1][i],
+            (x_value, y_values),
+            xytext=(4, 0),
+            textcoords="offset points",
+            va="center",
+            ha="left",
+        )
+    plt.savefig("stat.svg", bbox_inches="tight")
+    print("new image generated")
+
 
 def get_stats() -> list:
-    '''Gets API data and returns markdown progress'''
+    """Gets API data and returns markdown progress"""
     data = requests.get(
-        f"https://wakatime.com/api/v1/users/current/stats/last_7_days?api_key={waka_key}").json()
+        f"https://wakatime.com/api/v1/users/current/stats/last_7_days?api_key={waka_key}"
+    ).json()
 
     try:
-        lang_data = data['data']['languages']
+        lang_data = data["data"]["languages"]
     except KeyError:
-        print("Please Add your WakaTime API Key to the Repository Secrets")
+        print("error: please add your WakaTime API key to the Repository Secrets")
         sys.exit(1)
 
-    lang_list =[]
+    lang_list = []
     time_list = []
     percent_list = []
-    pad = len(max([l['name'] for l in lang_data[:5]], key=len))
+    pad = len(max([l["name"] for l in lang_data[:5]], key=len))
     for lang in lang_data[:5]:
-        lang_list.append(lang['name'])
-        time_list.append(lang['text'])
-        percent_list.append(lang['percent'])
-    data_list = [lang_list,time_list, percent_list]
-    print("Coding Data Collected")
+        lang_list.append(lang["name"])
+        time_list.append(lang["text"])
+        percent_list.append(lang["percent"])
+    data_list = [lang_list, time_list, percent_list]
+    print("coding data collected")
     return data_list
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     waka_stat = get_stats()
     make_graph(waka_stat)
-    print("Python Run Successful")
+    print("python script run successful")
